12a7cbb7525dff93701c66a7b949e30a90089aa4
Saul Lopez <saul.lopez@quixmati.com.mx>
2013-08-13 10:29:12 -0500

    Primera optimización de pruebas rspec!

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,22 +4,14 @@ describe 'Static pages' do
 
   let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
 
+  subject { page }
+
   describe 'Home page' do
+    before { visit root_path }
 
-    it "should have the content 'Sample App'" do
-      visit root_path
-      expect(page).to have_content('Sample App')
-    end
-
-    it 'should have the base title' do
-      visit root_path
-      expect(page).to have_title("#{base_title}")
-    end
-
-    it 'should not have a custom page title' do
-      visit root_path
-      expect(page).not_to have_title('| Home')
-    end
+    it { should have_content('Sample App') }
+    it { should have_title("#{base_title}") }
+    it { should_not have_title('| Home') }
   end
 
   describe 'Help page' do
