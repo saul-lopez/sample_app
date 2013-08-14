f87e17093bf47843e147538e94746f39ac55f534
Saul Lopez <saul.lopez@quixmati.com.mx>
2013-08-13 23:29:42 -0500

    Ejercisio 5.6.1 Se mejora con el uso de have_selector

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,7 +9,7 @@ describe 'Static pages' do
   describe 'Home page' do
     before { visit root_path }
 
-    it { should have_content('Sample App') }
+    it { should have_selector('h1', 'Sample App') }
     it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
   end
@@ -17,21 +17,21 @@ describe 'Static pages' do
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_content('Help') }
+    it { should have_selector('h1', 'Help') }
     it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it { should have_content('About') }
+    it { should have_selector('h1', 'About') }
     it { should have_title(full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
 
-    it { should have_content('Contact') }
+    it { should have_selector('h1', 'Contact') }
     it { should have_title(full_title('Contact')) }
   end
 
