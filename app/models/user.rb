57989563d8363576a22ed1076e10ce6e78caaf55
Saul Lopez <saul.lopez@quixmati.com.mx>
2013-08-15 18:38:12 -0500

    6.5.2. y 3 MEjora en validador de email. Refactor en downcase... Muy elegante

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
 
-  before_save { self.email = self.email.downcase }
+  before_save { self.email.downcase! }
 
-  VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email,
             presence: true,
             format: { with: VALID_EMAIL_REGEX },
