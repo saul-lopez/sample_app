97c371b84db03eefef70f44fdd9b24b49057abbf
Saul Lopez <saul.lopez@quixmati.com.mx>
2013-08-14 23:10:49 -0500

    Bugfix en full_title, faltaba la instrucción return para el caso en que se recibe cadena vacía. También se hace más tolerante esta función al interpretar solo espacios en blanco como vacío.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,11 @@ module ApplicationHelper
   # returns the full title on a per page basis
   def full_title(page_title)
     base_title = 'Ruby on Rails Tutorial Sample App'
-    if page_title.empty?
-      base_title
+    # Ojo empty regresa false para cadenas con solo espacios
+    # por eso se usa blank?:
+    # http://stackoverflow.com/questions/885414/a-concise-explanation-of-nil-v-empty-v-blank-in-ruby-on-rails
+    if page_title.blank?
+      return base_title
     end
     "#{base_title} | #{page_title}"
   end
