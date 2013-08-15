d0f9013ec58e4b7722fb4c6c566db1820b1e1d66
Saul Lopez <saul.lopez@quixmati.com.mx>
2013-08-14 20:06:46 -0500

    Validar presencia de los campos name y email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examles to (or delete) #{__FILE__}"
+
+  before { @user = User.new(name: 'Example User', email: 'yo@mi.net') }
+
+  subject { @user }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
 end
