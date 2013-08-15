3c0c8705988c6a8df755189a0a033584fc04103f
Saul Lopez <saul.lopez@quixmati.com.mx>
2013-08-14 20:23:54 -0500

    Validar presencia de los campos name y email. Pruebas

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,19 @@ describe User do
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+
+  it { should be_valid }
+
+  describe 'When name is not present' do
+    before { @user.name = ' ' }
+    it { should_not be_valid }
+  end
+
+  describe 'When email is not present' do
+    before { @user.email = ' ' }
+    it { should_not be_valid }
+  end
+
+  #subject { @user.name }
+  #it { should_not empty }
 end
