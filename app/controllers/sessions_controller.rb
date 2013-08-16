bf5b35ffbf0381b78cfea5ffc237523873817531
Saul Lopez <saul.lopez@quixmati.com.mx>
2013-08-16 12:23:00 -0500

    primeros pasos para autenticar usuarios

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if (user && user.authenticate(params[:session][:password]))
+      # Iniciar sesión y redirigir a página de perfil
+    else
+      # Crear mensaje de error y redirigir nuevamente al formulario de autenticación.
+      flash.now[:error] = 'Invalid user name or password'
+      # Se usa flash.now porque render no cuenta como un request y el flash se queda una
+      # etapa más de las deseadas (1)
+      render 'new'
+    end
   end
 
   def destroy
