ba89d4cdb2217a5afa770b42e5e4c25580c3ec9c
Saul Lopez <saul.lopez@quixmati.com.mx>
2013-08-15 22:43:21 -0500

    Formulario inicial para crear usuario. Se crea partial para despliegue de errores.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render 'new'
+    end
+  end
+
+  private
+    # Nuevo método que ofrece más seguridad, solo usar para la creación los
+    # parámetros explícitamente permitidos. Evitar por ejemplo que se envíe is_admin=1
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -18,4 +18,32 @@ describe 'User Pages' do
     it { should have_selector('h1', user.name) }
     it { should have_title(user.name) }
   end
+
+  describe 'signup' do
+    before { visit signup_path }
+    let(:submit) { 'Create my account' }
+
+    describe 'wiith invalid information' do
+      it 'should not create a user' do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+      it { should have_selector('li', "Name can't be blank") }
+      it { should have_selector('li', "Email can't be blank") }
+      it { should have_selector('li', "Email is invalid") }
+      it { should have_selector('li', "Password can't be blank") }
+      it { should have_selector('li', "Password is too short (minimum is 6 characters)") }
+    end
+
+    describe 'with valid informatio' do
+      before do
+        fill_in 'Name',          with: 'Example User'
+        fill_in 'Email',         with: 'user@example.com'
+        fill_in 'Password',      with: 'foobar'
+        fill_in 'Confirmation',  with: 'foobar'
+      end
+      it 'should create a user' do
+        expect {click_button submit }.to change(User, :count).by(1)
+      end
+    end
+  end
 end
