cf907ea6c955ac7bb5b07a14596ac8155462351b
Saul Lopez <saul.lopez@quixmati.com.mx>
2013-08-16 11:56:23 -0500

    Esqueleto para gestión de sesiones.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,16 @@ SampleApp::Application.routes.draw do
 
   # Crea todas las rutas relativas a los recursos REST : Sección 7.1.2
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   root  'static_pages#home'
 
   match '/signup',  to: 'users#new',            via: 'get'
+
+  # Personalizar el nombre de las rutas para crear y destruir sesión:
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
